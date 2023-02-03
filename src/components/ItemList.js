c318f209120197da5f5c74e5d3c2f1d513bf088e
German Vespa <german.vespagut@gmail.com>
2023-02-03 13:00:16 -0300

    try

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,107 +1,3 @@
-// import Item from "./Item"
-// import { useState, useEffect } from "react"
-// import Card from 'react-bootstrap/Card'
-
-
-
-
-
-// const ItemList = ({ productos }) => {
-
-//     const [productosFiltrados, setProductosFiltrados] = useState(productos)
-//     const [txtFilterValue, setTxtFilterValue] = useState()
-//     const [selectValue, setSelectValue] = useState()
-
-//     useEffect(() => {
-
-//         // console.log()
-//         setProductosFiltrados(productos);
-//     }, [])
-
-//     const handleFilter = e => {
-//         console.log(e.target.value)
-//         const filterType = e.target.dataset.filter;
-
-//         if ((filterType == 'nombre' && !e.target.value && !selectValue) || (filterType == 'variedad' && !txtFilterValue && e.target.value == "todos")) {
-//             setProductosFiltrados(productos);
-//             return ;
-//         }
-//         const copia = productos.filter(producto => {
-//             if (e.target.dataset.filter == "nombre") {
-//                 setTxtFilterValue(e.target.value)
-//                 if (e.target.value && selectValue != "Todos") {
-//                     if (producto.nombre.toLowerCase().includes(e.target.value.toLowerCase()) && producto.variedad == selectValue) {
-//                         return producto;
-//                     }
-//                 } else if(!e.target.value && selectValue != "Todos"){
-//                     if (producto.variedad == selectValue) {
-//                         return producto;
-//                     }
-//                 } else {
-//                     if (producto.nombre.toLowerCase().includes(e.target.value.toLowerCase())) {
-//                         return producto;
-//                     }
-//                 }
-//             }
-
-//             if (e.target.dataset.filter == "variedad") {
-//                 setSelectValue(e.target.value)
-//                 if (txtFilterValue && e.target.value == "Todos") {
-//                     if (producto.nombre.toLowerCase().includes(txtFilterValue.toLowerCase())) {
-//                         return producto;
-//                     }
-//                 }  else if (txtFilterValue && e.target.value != "Todos") {
-//                     if (producto.nombre.toLowerCase().includes(txtFilterValue.toLowerCase()) && producto.variedad == e.target.value) {
-//                         return producto;
-//                     }
-//                 } else if (!txtFilterValue && e.target.value != "Todos") {
-//                     if (producto.variedad == e.target.value) {
-//                         return producto;
-//                     }
-//                 }
-//             }
-//         })
-//         setProductosFiltrados(copia)
-//     }
-
-//     const handleChangeName = (e) => {
-//         console.log(e.target.value)
-//         const copia = productos.filter(producto => {
-//             return producto.nombre.toLowerCase().includes(e.target.value.toLowerCase())
-//         })
-//         setProductosFiltrados(copia)
-//     }
-
-
-//     return (
-//         <>
-//             <nav>
-//                 <input data-filter="nombre" onChange={handleFilter} type="text" placeholder="Nombre del producto"></input>
-
-//                 <select data-filter="variedad" onChange={handleFilter}>
-//                     <option value="Todos">Todos</option>
-//                     <option value="Sativa">Sativa</option>
-//                     <option value="Indica">Indica</option>
-
-//                 </select>
-//             </nav>
-
-//             <div className="row">
-//                 {productosFiltrados?.map((producto) => {
-//                     return (
-//                         <Item producto={producto} key={producto.id} />
-
-
-//                     )
-//                 })}
-//             </div>
-//         </>
-//     )
-// }
-
-// export default ItemList
-
-
 import Item from "./Item"
 import { useState, useEffect } from "react"
 import Card from 'react-bootstrap/Card'
@@ -110,14 +6,85 @@ import Card from 'react-bootstrap/Card'
 
 
 
-const ItemList = ({productos}) => {
-    
+const ItemList = ({ productos }) => {
+
     const [productosFiltrados, setProductosFiltrados] = useState(productos)
+    const [txtFilterValue, setTxtFilterValue] = useState()
+    const [selectValue, setSelectValue] = useState()
+
+    useEffect(() => {
+
+        // console.log()
+        setProductosFiltrados(productos);
+    }, [])
+
+    const handleFilter = e => {
+        console.log(e.target.value)
+        const filterType = e.target.dataset.filter;
+
+        if ((filterType == 'nombre' && !e.target.value && !selectValue) || (filterType == 'variedad' && !txtFilterValue && e.target.value == "todos")) {
+            setProductosFiltrados(productos);
+            return ;
+        }
+        const copia = productos.filter(producto => {
+            if (e.target.dataset.filter == "nombre") {
+                setTxtFilterValue(e.target.value)
+                if (e.target.value && selectValue != "Todos") {
+                    if (producto.nombre.toLowerCase().includes(e.target.value.toLowerCase()) && producto.variedad == selectValue) {
+                        return producto;
+                    }
+                } else if(!e.target.value && selectValue != "Todos"){
+                    if (producto.variedad == selectValue) {
+                        return producto;
+                    }
+                } else {
+                    if (producto.nombre.toLowerCase().includes(e.target.value.toLowerCase())) {
+                        return producto;
+                    }
+                }
+            }
+
+            if (e.target.dataset.filter == "variedad") {
+                setSelectValue(e.target.value)
+                if (txtFilterValue && e.target.value == "Todos") {
+                    if (producto.nombre.toLowerCase().includes(txtFilterValue.toLowerCase())) {
+                        return producto;
+                    }
+                }  else if (txtFilterValue && e.target.value != "Todos") {
+                    if (producto.nombre.toLowerCase().includes(txtFilterValue.toLowerCase()) && producto.variedad == e.target.value) {
+                        return producto;
+                    }
+                } else if (!txtFilterValue && e.target.value != "Todos") {
+                    if (producto.variedad == e.target.value) {
+                        return producto;
+                    }
+                }
+            }
+        })
+        setProductosFiltrados(copia)
+    }
+
+    const handleChangeName = (e) => {
+        console.log(e.target.value)
+        const copia = productos.filter(producto => {
+            return producto.nombre.toLowerCase().includes(e.target.value.toLowerCase())
+        })
+        setProductosFiltrados(copia)
+    }
 
 
     return (
         <>
+            <nav>
+                <input data-filter="nombre" onChange={handleFilter} type="text" placeholder="Nombre del producto"></input>
 
+                <select data-filter="variedad" onChange={handleFilter}>
+                    <option value="Todos">Todos</option>
+                    <option value="Sativa">Sativa</option>
+                    <option value="Indica">Indica</option>
+
+                </select>
+            </nav>
 
             <div className="row">
                 {productosFiltrados?.map((producto) => {
@@ -133,3 +100,36 @@ const ItemList = ({productos}) => {
 }
 
 export default ItemList
+
+
+// import Item from "./Item"
+// import { useState, useEffect } from "react"
+// import Card from 'react-bootstrap/Card'
+
+
+
+
+
+// const ItemList = ({productos}) => {
+    
+//     const [productosFiltrados, setProductosFiltrados] = useState(productos)
+
+
+//     return (
+//         <>
+
+
+//             <div className="row">
+//                 {productosFiltrados?.map((producto) => {
+//                     return (
+//                         <Item producto={producto} key={producto.id} />
+
+
+//                     )
+//                 })}
+//             </div>
+//         </>
+//     )
+// }
+
+// export default ItemList
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,52 +1,3 @@
-// import { useEffect, useState } from 'react';
-// import ItemList from './ItemList';
-// import { useParams } from 'react-router-dom';
-// import { geneticas } from './data.js';
-
-
-
-// const ItemListContainer = () => {
-
-
-
-
-
-//     const [load, setLoad] = useState(true) // no parte desde false ya que no estoy trayendo desde URL sino la datra desde un json local. por ende no hay espera.
-//     const [productos, setProductos] = useState([])
-
-//     const props = useParams()
-
-
-//     useEffect(() => {
-
-
-//         const productos = geneticas
-//         console.log(geneticas)
-
-//         setProductos(productos)
-
-
-//     }, [])
-
-
-
-
-
-//     return (
-//         <>
-
-//             {load ? <p className="itemListContainer__par">Aqui se encuentran las geneticas que pueden elegir los socios para incluir en su cuota de este mes:</p> : "Cargando productos..."}
-
-//             <ItemList productos={productos} />
-
-//         </>
-//     )
-// }
-
-// export default ItemListContainer;
-
-
-
 import { useEffect, useState } from 'react';
 import ItemList from './ItemList';
 import { useParams } from 'react-router-dom';
@@ -63,96 +14,27 @@ const ItemListContainer = () => {
     const [load, setLoad] = useState(true) // no parte desde false ya que no estoy trayendo desde URL sino la datra desde un json local. por ende no hay espera.
     const [productos, setProductos] = useState([])
 
-    const [productosFiltrados, setProductosFiltrados] = useState(productos)
-    const [txtFilterValue, setTxtFilterValue] = useState()
-    const [selectValue, setSelectValue] = useState()
-
     const props = useParams()
 
 
     useEffect(() => {
 
+
         const productos = geneticas
-        console.log(productos)
+        console.log(geneticas)
 
         setProductos(productos)
 
-        setProductosFiltrados(productos);
 
     }, [])
 
 
 
 
-    const handleFilter = e => {
-        console.log(e.target.value)
-        const filterType = e.target.dataset.filter;
-
-        if ((filterType == 'nombre' && !e.target.value && !selectValue) || (filterType == 'variedad' && !txtFilterValue && e.target.value == "todos")) {
-            setProductosFiltrados(productos);
-            return;
-        }
-        const copia = productos.filter(producto => {
-            if (e.target.dataset.filter == "nombre") {
-                setTxtFilterValue(e.target.value)
-                if (e.target.value && selectValue != "Todos") {
-                    if (producto.nombre.toLowerCase().includes(e.target.value.toLowerCase()) && producto.variedad == selectValue) {
-                        return producto;
-                    }
-                } else if (!e.target.value && selectValue != "Todos") {
-                    if (producto.variedad == selectValue) {
-                        return producto;
-                    }
-                } else {
-                    if (producto.nombre.toLowerCase().includes(e.target.value.toLowerCase())) {
-                        return producto;
-                    }
-                }
-            }
-
-            if (e.target.dataset.filter == "variedad") {
-                setSelectValue(e.target.value)
-                if (txtFilterValue && e.target.value == "Todos") {
-                    if (producto.nombre.toLowerCase().includes(txtFilterValue.toLowerCase())) {
-                        return producto;
-                    }
-                } else if (txtFilterValue && e.target.value != "Todos") {
-                    if (producto.nombre.toLowerCase().includes(txtFilterValue.toLowerCase()) && producto.variedad == e.target.value) {
-                        return producto;
-                    }
-                } else if (!txtFilterValue && e.target.value != "Todos") {
-                    if (producto.variedad == e.target.value) {
-                        return producto;
-                    }
-                }
-            }
-        })
-        setProductosFiltrados(copia)
-    }
-
-    const handleChangeName = (e) => {
-        console.log(e.target.value)
-        const copia = productos.filter(producto => {
-            return producto.nombre.toLowerCase().includes(e.target.value.toLowerCase())
-        })
-        setProductosFiltrados(copia)
-    }
-
-
-
 
     return (
         <>
-            <nav>
-                <input data-filter="nombre" onChange={handleFilter} type="text" placeholder="Nombre del producto"></input>
 
-                <select data-filter="variedad" onChange={handleFilter}>
-                    <option value="Todos">Todos</option>
-                    <option value="Sativa">Sativa</option>
-                    <option value="Indica">Indica</option>
-
-                </select>
-            </nav>
             {load ? <p className="itemListContainer__par">Aqui se encuentran las geneticas que pueden elegir los socios para incluir en su cuota de este mes:</p> : "Cargando productos..."}
 
             <ItemList productos={productos} />
@@ -162,5 +44,123 @@ const ItemListContainer = () => {
 }
 
 export default ItemListContainer;
+
+
+
+// import { useEffect, useState } from 'react';
+// import ItemList from './ItemList';
+// import { useParams } from 'react-router-dom';
+// import { geneticas } from './data.js';
+
+
+
+// const ItemListContainer = () => {
+
+
+
+
+
+//     const [load, setLoad] = useState(true) // no parte desde false ya que no estoy trayendo desde URL sino la datra desde un json local. por ende no hay espera.
+//     const [productos, setProductos] = useState([])
+
+//     const [productosFiltrados, setProductosFiltrados] = useState(productos)
+//     const [txtFilterValue, setTxtFilterValue] = useState()
+//     const [selectValue, setSelectValue] = useState()
+
+//     const props = useParams()
+
+
+//     useEffect(() => {
+
+//         const productos = geneticas
+//         console.log(productos)
+
+//         setProductos(productos)
+
+//         setProductosFiltrados(productos);
+
+//     }, [])
+
+
+
+
+//     const handleFilter = e => {
+//         console.log(e.target.value)
+//         const filterType = e.target.dataset.filter;
+
+//         if ((filterType == 'nombre' && !e.target.value && !selectValue) || (filterType == 'variedad' && !txtFilterValue && e.target.value == "todos")) {
+//             setProductosFiltrados(productos);
+//             return;
+//         }
+//         const copia = productos.filter(producto => {
+//             if (e.target.dataset.filter == "nombre") {
+//                 setTxtFilterValue(e.target.value)
+//                 if (e.target.value && selectValue != "Todos") {
+//                     if (producto.nombre.toLowerCase().includes(e.target.value.toLowerCase()) && producto.variedad == selectValue) {
+//                         return producto;
+//                     }
+//                 } else if (!e.target.value && selectValue != "Todos") {
+//                     if (producto.variedad == selectValue) {
+//                         return producto;
+//                     }
+//                 } else {
+//                     if (producto.nombre.toLowerCase().includes(e.target.value.toLowerCase())) {
+//                         return producto;
+//                     }
+//                 }
+//             }
+
+//             if (e.target.dataset.filter == "variedad") {
+//                 setSelectValue(e.target.value)
+//                 if (txtFilterValue && e.target.value == "Todos") {
+//                     if (producto.nombre.toLowerCase().includes(txtFilterValue.toLowerCase())) {
+//                         return producto;
+//                     }
+//                 } else if (txtFilterValue && e.target.value != "Todos") {
+//                     if (producto.nombre.toLowerCase().includes(txtFilterValue.toLowerCase()) && producto.variedad == e.target.value) {
+//                         return producto;
+//                     }
+//                 } else if (!txtFilterValue && e.target.value != "Todos") {
+//                     if (producto.variedad == e.target.value) {
+//                         return producto;
+//                     }
+//                 }
+//             }
+//         })
+//         setProductosFiltrados(copia)
+//     }
+
+//     const handleChangeName = (e) => {
+//         console.log(e.target.value)
+//         const copia = productos.filter(producto => {
+//             return producto.nombre.toLowerCase().includes(e.target.value.toLowerCase())
+//         })
+//         setProductosFiltrados(copia)
+//     }
+
+
+
+
+//     return (
+//         <>
+//             <nav>
+//                 <input data-filter="nombre" onChange={handleFilter} type="text" placeholder="Nombre del producto"></input>
+
+//                 <select data-filter="variedad" onChange={handleFilter}>
+//                     <option value="Todos">Todos</option>
+//                     <option value="Sativa">Sativa</option>
+//                     <option value="Indica">Indica</option>
+
+//                 </select>
+//             </nav>
+//             {load ? <p className="itemListContainer__par">Aqui se encuentran las geneticas que pueden elegir los socios para incluir en su cuota de este mes:</p> : "Cargando productos..."}
+
+//             <ItemList productos={productos} />
+
+//         </>
+//     )
+// }
+
+// export default ItemListContainer;
 
 
